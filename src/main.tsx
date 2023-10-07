c179f85a81e3adee7fa67888a14c6a0e9206cf0e
Samuel Pressé-Lefebvre <samuelpresse@gmail.com>
2023-10-07 14:33:25 -0400

    Build the color mode switch

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
-import theme from "./theme";
+import theme from "./Theme";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
