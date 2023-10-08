6106cea353914a0aaa6e28c167d15ef07a58769b
Samuel Pressé-Lefebvre <samuelpresse@gmail.com>
2023-10-07 22:12:34 -0400

    Show loading skeletons

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key:'689c11d919cc4ae88137661dea9bb654'
-    }
+        key:'689c11d919cc4ae88137661dea9bb654',
+    },
 });
